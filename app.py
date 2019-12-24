606d5fe5e93c90864648205203dba9f3b984a109
Yan Ke <2311193037@qq.com>
2019-12-24 15:04:06 +0800

    feat: customer related feature complete
    Please test and report bugs, should they arise.
    
    More css beautify work needs to be done:
    
     - the intro is too long for some books, maybe try text-overflow or something
     - customer.html needs beautify
     - plz try avoiding white font color on pale blue background, it's kind of hard to see clearly
    
    Also, it would be nice if someone could come up with another way to convert RedemptionPoints to discount. It is not urgent, just, current method is a bit too simple.

--- app.py
+++ app.py
@@ -22,6 +22,16 @@ def insert(sql_query):
     con.commit()
 
 
+# 积分转化为折扣
+def rp2dis(rp):
+    if rp < 100:
+        return 0.9
+    if rp < 200:
+        return 0.85
+    if rp < 400:
+        return 0.8
+
+
 # 初始登录界面
 @app.route('/')
 def startup():
@@ -103,11 +113,6 @@ def login():
         return render_template('login.html')
 
 
-'''# 图书主页
-@app.route('/book/<artv>', methods=['GET','POST'])
-def book(argv):
-'''
-
 # sql查询
 @app.route('/search_book/<argv>', methods=['GET','POST'])
 def search_book(argv):
@@ -116,26 +121,19 @@ def search_book(argv):
     query_txt=""
     if argv == 'cart':
         if request.method == 'POST':
-            chkbox_values = request.form.getlist('chkbox')
-            quantity_values = request.form.getlist('quantity')
-            branch_values = request.form.getlist('branch')
-            if len(chkbox_values) != 0:
-                sql_query = "SELECT max(OrderID) FROM `order`"
-                OrderID = select(sql_query)[0][0]+1
-                print(OrderID)
-                sql_query = "SELECT CustomerID FROM customer WHERE CustomerName='"+customer+"';"
-                CustomerID = select(sql_query)[0][0]
-                today=datetime.date.today()
-                sql_query = "INSERT IGNORE INTO `order` VALUES ('"+str(OrderID)+"', '"+str(CustomerID)+"', '"+str(today)+"','in cart', NULL, NULL)"
-                insert(sql_query)
-            print(chkbox_values)
-            for i in range(len(chkbox_values)):
-                ISBN = chkbox_values[i]
-                quantity = quantity_values[i]
-                branch = branch_values[i]
-                sql_query = "INSERT INTO `order_entry` VALUES ('"+str(OrderID)+"', '"+str(ISBN)+"', '"+str(branch)+"', '"+str(quantity)+"', 1.0)"
-                insert(sql_query)
-            print(request.referrer)
+            sql_query = "SELECT max(OrderID) FROM `order`"
+            OrderID = select(sql_query)[0][0]+1
+            print(OrderID)
+            sql_query = "SELECT CustomerID FROM customer WHERE CustomerName='"+customer+"';"
+            CustomerID = select(sql_query)[0][0]
+            today=datetime.date.today()
+            sql_query = "INSERT IGNORE INTO `order` VALUES ('"+str(OrderID)+"', '"+str(CustomerID)+"', '"+str(today)+"','in cart', NULL, NULL)"
+            insert(sql_query)
+            ISBN = session['ISBN']
+            quantity = request.form['quantity']
+            branch = request.form['branch']
+            sql_query = "INSERT INTO `order_entry` VALUES ('"+str(OrderID)+"', '"+str(ISBN)+"', '"+str(branch)+"', '"+str(quantity)+"', 1.0)"
+            insert(sql_query)
             return redirect(url_for('search_book', argv = 'search'))
 
     if argv == 'search':
@@ -186,10 +184,13 @@ def search_book(argv):
 # 图书主页
 @app.route('/book/<argv>', methods=['GET','POST'])
 def book(argv):
+    print(request.url)
+    session['ISBN'] = argv
     sql_query="SELECT book.ISBN,book.Title,group_concat(DISTINCT compose.AuthorName) AS AuthorName,"\
     "book.Publisher,book.Price,book.ChineseIntro,book.EnglishIntro FROM book,compose WHERE book.ISBN LIKE "+\
     argv+" AND compose.ISBN = book.ISBN GROUP BY book.ISBN;"
     return render_template('bookinfo.html', data = select(sql_query), customer=customer)
+
 
 # 客户信息显示
 @app.route('/customer/<argv>', methods=['GET','POST'])
@@ -200,21 +201,37 @@ def Customer(argv):
     elif argv == "cart":
         sql_query = "SELECT CustomerID FROM customer WHERE CustomerName='"+customer+"';"
         CustomerID = select(sql_query)[0][0]
-        sql_query = "SELECT order_entry.*,order.Date,order.Message,order.Reply,order.Status FROM order_entry INNER JOIN `order` ON order.OrderID = order_entry.OrderID WHERE order.Status = 'in cart' AND order.CustomerID = " + str(CustomerID) + ";"
+        sql_query = "SELECT order_entry.*,order.Date,order.Message,order.Reply,order.Status FROM order_entry INNER JOIN `order` ON order.OrderID = order_entry.OrderID WHERE order.Status = 'in cart' AND order.CustomerID = " + str(CustomerID) + " ;"
         data = select(sql_query)
-        html_str = ""
+        html_str = '<form action="/customer/cart" method="POST">'
         for record in data:
             html_str += '<table> <div class="mytable">'
-            html_str += f'<tr><th>订单号:</th><td>{record[0]}</td></tr>'
+            html_str += f'<tr><th><input type="checkbox" name="chkbox" value="{record[0]}">订单号:</th><td>{record[0]}</td></tr>'
             html_str += f'<tr><th>ISBN:</th><td>{record[1]}</td></tr>'
             html_str += f'<tr><th>门店:</th><td>{record[2]}</td></tr>'
             html_str += f'<tr><th>数量:</th><td>{record[3]}</td></tr>'
-            html_str += f'<tr><th>折扣:</th><td>{record[4]}</td></tr>'
-            html_str += f'<tr><th>日期:</th><td>{record[5]}</td></tr>'
-            html_str += f'<tr><th>留言:</th><td>{record[6]}</td></tr>'
-            html_str += f'<tr><th>回复:</th><td>{record[7]}</td></tr>'
-            html_str += f'<tr><th>状态:</th><td>{record[8]}</td></tr>'
             html_str += '</div></table></br>'
+        html_str += '<input type="submit" value="下单"></form>'
+        if request.method == 'POST':
+            chkbox_values = request.form.getlist('chkbox')
+            if len(chkbox_values) != 0:
+                sql_query = "SELECT max(OrderID) FROM `order`"
+                OrderID = select(sql_query)[0][0]+1
+                print(OrderID)
+                today = datetime.date.today()
+                sql_query = "INSERT IGNORE INTO `order` VALUES ('"+str(OrderID)+"', '"+str(CustomerID)+"', '"+str(today)+"','undone', NULL, NULL)"
+                insert(sql_query)
+                for chkbox_value in chkbox_values:
+                    sql_query = "SELECT CustomerID FROM customer WHERE CustomerName='"+customer+"';"
+                    CustomerID = select(sql_query)[0][0]   
+                    sql_query = "SELECT RedemptionPoints FROM customer WHERE CustomerName='"+customer+"';"
+                    RedemptionPoints = int(select(sql_query)[0][0])
+                    Discount = rp2dis(RedemptionPoints)
+                    sql_query = "UPDATE `order_entry` SET Discount = "+ str(Discount) +", OrderID = " + str(OrderID) + " WHERE OrderID = '" + chkbox_value + "';"
+                    insert(sql_query)
+                    sql_query = "DELETE FROM `order` WHERE OrderID = " + chkbox_value + ";"
+                    insert(sql_query)
+            return redirect(url_for('Customer', argv = 'cart'))
         return render_template('customer.html', content = html_str, customer = customer)
     elif argv == "info":
         sql_query = "SELECT * FROM customer WHERE CustomerName='"+customer+"';"
@@ -222,30 +239,118 @@ def Customer(argv):
         html_str = ""
         for record in data:
             html_str += '<table> <div class="mytable">'
-            html_str += f'<tr><th>ID:</th><td>{record[0]}</td></tr>'
-            html_str += f'<tr><th>用户名:</th><td>{record[1]}</td></tr>'
-            html_str += f'<tr><th>积分:</th><td>{record[3]}</td></tr>'
+            html_str += f'<tr><th>ID:</th><th>用户名:</th><th>积分:</th></tr>'
+            html_str += f'<tr><td>{record[0]}</td><td>{record[1]}</td><td>{record[3]}</td></tr>'
             html_str += '</div></table></br>'
         return render_template('customer.html', content = html_str, customer = customer)
     elif argv == "history":
         sql_query = "SELECT CustomerID FROM customer WHERE CustomerName='"+customer+"';"
         CustomerID = select(sql_query)[0][0]
-        sql_query = "SELECT order_entry.*,order.Date,order.Message,order.Reply,order.Status FROM order_entry INNER JOIN `order` ON order.OrderID = order_entry.OrderID WHERE order.Status = 'done' AND order.CustomerID = " + str(CustomerID) + ";"
+        sql_query = "SELECT * FROM `order` WHERE order.Status != 'in cart' AND order.CustomerID = " + str(CustomerID) + ";"
         data = select(sql_query)
         html_str = ""
         for record in data:
             html_str += '<table> <div class="mytable">'
-            html_str += f'<tr><th>订单号:</th><td>{record[0]}</td></tr>'
-            html_str += f'<tr><th>ISBN:</th><td>{record[1]}</td></tr>'
-            html_str += f'<tr><th>门店:</th><td>{record[2]}</td></tr>'
-            html_str += f'<tr><th>数量:</th><td>{record[3]}</td></tr>'
-            html_str += f'<tr><th>折扣:</th><td>{record[4]}</td></tr>'
-            html_str += f'<tr><th>日期:</th><td>{record[5]}</td></tr>'
-            html_str += f'<tr><th>留言:</th><td>{record[6]}</td></tr>'
-            html_str += f'<tr><th>回复:</th><td>{record[7]}</td></tr>'
-            html_str += f'<tr><th>状态:</th><td>{record[8]}</td></tr>'
+            html_str += f'<tr><th>订单号:</th><td><a href="/historyorder/{record[0]}" target="_parent"><span>{record[0]}</span></a></td></tr>'
+            html_str += f'<tr><th>日期:</th><td>{record[2]}</td></tr>'
+            html_str += f'<tr><th>留言:</th><td>{record[4]}</td></tr>'
+            html_str += f'<tr><th>回复:</th><td>{record[5]}</td></tr>'
+            html_str += f'<tr><th>状态:</th><td>{record[3]}</td></tr>'
             html_str += '</div></table></br>'
         return render_template('customer.html', content = html_str, customer = customer)
+    elif argv == "changepw":
+        html_str = '<form class="top-info" action="/customer/changepw" method="POST">'
+        html_str += f'<input type="text" name="newpw1"></br>'
+        html_str += f'<input type="text" name="newpw2"></br>'
+        html_str += f'<input type="submit" value="修改密码"></br>'
+        html_str += f'</form>'
+        if request.method == 'POST':
+            pw1 = request.form['newpw1']
+            pw2 = request.form['newpw2']
+            sql = "SELECT * FROM customer WHERE CustomerName = '"+customer+"';"  
+            tmp = select(sql)
+            if len(tmp) == 0:
+                print('用户名不存在')
+            elif pw1 != pw2:
+                print('密码不一致，请重新输入')
+            elif 6 > len(pw1) or len(pw1) > 15:
+                print('密码需由6~15个字符组成，请重新输入')
+            else:
+                print('密码修改成功')
+                cur = con.cursor()
+                cur.execute("UPDATE customer SET Password = '" + hashlib.md5(pw1.encode("utf-8")).hexdigest() + "' WHERE CustomerName = '" + customer + "';")
+                con.commit()
+                return render_template('customer.html', content = html_str, customer = customer)
+        return render_template('customer.html', content = html_str, customer = customer)
+    elif argv == "message":
+        sql_query = "SELECT CustomerID FROM customer WHERE CustomerName='"+customer+"';"
+        CustomerID = select(sql_query)[0][0]
+        sql_query = "SELECT * FROM `order` WHERE order.CustomerID = " + str(CustomerID) + ";"
+        data = select(sql_query)
+        html_str = ""
+        for record in data:
+            html_str += '<table> <div class="mytable">'
+            html_str += f'<tr><th>订单号:</th><td><a href="/messageorder/{record[0]}" target="_parent"><span>{record[0]}</span></a></td></tr>'
+            html_str += f'<tr><th>日期:</th><td>{record[2]}</td></tr>'
+            html_str += f'<tr><th>留言:</th><td>{record[4]}</td></tr>'
+            html_str += f'<tr><th>回复:</th><td>{record[5]}</td></tr>'
+            html_str += f'<tr><th>状态:</th><td>{record[3]}</td></tr>'
+            html_str += '</div></table></br>'
+        if request.method == 'POST':
+            message = request.form['message']
+            sql_query = "UPDATE `order` SET Message = '" + message + "' WHERE OrderID = '" + session['MesOrderID'] + "';"
+            insert(sql_query)
+            return redirect(url_for('Customer', argv = 'message'))
+        return render_template('customer.html', content = html_str, customer = customer)
+    else:
+        return render_template('customer.html', customer = customer)
+
+
+@app.route('/messageorder/<argv>', methods = ['GET','POST'])
+def messageorder(argv):
+    sql_query = "SELECT CustomerID FROM customer WHERE CustomerName='"+customer+"';"
+    CustomerID = select(sql_query)[0][0]
+    sql_query = "SELECT order_entry.*,order.Date,order.Message,order.Reply,order.Status FROM order_entry INNER JOIN `order` ON order.OrderID = order_entry.OrderID WHERE order.CustomerID = " + str(CustomerID) + " AND order.OrderID = " + argv + " ;"
+    data = select(sql_query)
+    html_str = '<form action="/customer/message" method="POST">'
+    for record in data:
+        html_str += '<table> <div class="mytable">'
+        html_str += f'<tr><th>订单号:</th><td>{record[0]}</td></tr>'
+        html_str += f'<tr><th>ISBN:</th><td>{record[1]}</td></tr>'
+        html_str += f'<tr><th>门店:</th><td>{record[2]}</td></tr>'
+        html_str += f'<tr><th>数量:</th><td>{record[3]}</td></tr>'
+        html_str += f'<tr><th>折扣:</th><td>{record[4]}</td></tr>'
+        html_str += f'<tr><th>日期:</th><td>{record[5]}</td></tr>'
+        html_str += f'<tr><th>留言:</th><td>{record[6]}</td></tr>'
+        html_str += f'<tr><th>回复:</th><td>{record[7]}</td></tr>'
+        html_str += f'<tr><th>状态:</th><td>{record[8]}</td></tr>'
+        html_str += '</div></table></br>'
+    html_str += '<input class="input-txt" name="message" type="text" placeholder="新留言">'
+    html_str += '<input type="submit" value="修改"></form>'
+    session['MesOrderID'] = argv
+    return render_template('customer.html', content = html_str, customer = customer)
+
+
+@app.route('/historyorder/<argv>', methods = ['GET','POST'])
+def historyorder(argv):
+    sql_query = "SELECT CustomerID FROM customer WHERE CustomerName='"+customer+"';"
+    CustomerID = select(sql_query)[0][0]
+    sql_query = "SELECT order_entry.*,order.Date,order.Message,order.Reply,order.Status FROM order_entry INNER JOIN `order` ON order.OrderID = order_entry.OrderID WHERE order.CustomerID = " + str(CustomerID) + " AND order.OrderID = " + argv + " ;"
+    data = select(sql_query)
+    html_str = ""
+    for record in data:
+        html_str += '<table> <div class="mytable">'
+        html_str += f'<tr><th>订单号:</th><td>{record[0]}</td></tr>'
+        html_str += f'<tr><th>ISBN:</th><td>{record[1]}</td></tr>'
+        html_str += f'<tr><th>门店:</th><td>{record[2]}</td></tr>'
+        html_str += f'<tr><th>数量:</th><td>{record[3]}</td></tr>'
+        html_str += f'<tr><th>折扣:</th><td>{record[4]}</td></tr>'
+        html_str += f'<tr><th>日期:</th><td>{record[5]}</td></tr>'
+        html_str += f'<tr><th>留言:</th><td>{record[6]}</td></tr>'
+        html_str += f'<tr><th>回复:</th><td>{record[7]}</td></tr>'
+        html_str += f'<tr><th>状态:</th><td>{record[8]}</td></tr>'
+        html_str += '</div></table></br>'
+    return render_template('customer.html', content = html_str, customer = customer)
 
 
 if __name__ == "__main__":
